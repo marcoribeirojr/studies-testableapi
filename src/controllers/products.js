4231d03c18443ffb2dd43c904cf36b4c9e932aa3
Marco Ribeiro Jr <mribeirodev@gmail.com>
2017-08-07 00:09:51 -0300

    First unit test

---
+++ src/controllers/products.js
@@ -0,0 +1,14 @@
+'use strict'
+
+class ProductsController {
+
+  get(req, res) {
+    return res.send([{
+        name : 'Default product'
+      , description : 'product description'
+      , price : 100
+    }])
+  }
+}
+
+export default ProductsController
